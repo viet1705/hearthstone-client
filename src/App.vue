785e7b6778ca8b0643089518010946ff13b30950
Viet Nguyen <viet.nd@anlab.pro>
2021-06-18 17:02:32 +0700

    init app

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <AppNavigation />
+  <RouterView />
+  <AppFooter />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AppFooter from './components/AppFooter.vue'
+import AppNavigation from './components/AppNavigation.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    AppNavigation,
+    AppFooter,
+  },
+})
+</script>
---
+++ src/pages/Login.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">
+            Sign in
+          </h1>
+          <p class="text-xs-center">
+            <AppLink name="register">
+              Need an account?
+            </AppLink>
+          </p>
+
+          <ul class="error-messages">
+            <li
+              v-for="(error, field) in errors"
+              :key="field"
+            >
+              {{ field }} {{ error ? error[0] : '' }}
+            </li>
+          </ul>
+
+          <form
+            ref="formRef"
+            @submit.prevent="login"
+          >
+            <fieldset
+              class="form-group"
+              aria-required="true"
+            >
+              <input
+                v-model="form.email"
+                class="form-control form-control-lg"
+                type="email"
+                required
+                placeholder="Email"
+              >
+            </fieldset>
+            <fieldset class=" form-group">
+              <input
+                v-model="form.password"
+                class="form-control form-control-lg"
+                type="password"
+                required
+                placeholder="Password"
+              >
+            </fieldset>
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="!form.email || !form.password"
+              type="submit"
+            >
+              Sign in
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { routerPush } from 'src/router'
+import { postLogin, PostLoginErrors, PostLoginForm } from 'src/services/auth/postLogin'
+import { updateUser } from 'src/store/user'
+import { defineComponent, reactive, ref } from 'vue'
+
+export default defineComponent({
+  name: 'LoginPage',
+  setup () {
+    const formRef = ref<HTMLFormElement | null>(null)
+    const form = reactive<PostLoginForm>({
+      email: '',
+      password: '',
+    })
+
+    const errors = ref<PostLoginErrors>({})
+
+    const login = async () => {
+      if (!formRef.value?.checkValidity()) return
+
+      const result = await postLogin(form)
+      if (result.isOk()) {
+        updateUser(result.value)
+        await routerPush('global-feed')
+      } else {
+        errors.value = await result.value.getErrors()
+      }
+    }
+
+    return {
+      formRef,
+      form,
+      login,
+      errors,
+    }
+  },
+})
+</script>
